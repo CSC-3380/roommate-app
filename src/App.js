b83305e490005d78832adb36041cec5255f62828
Kantonio Brownlee <kantoniosean@yahoo.com>
2022-04-02 17:49:01 -0500

    new

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import ChoreList from './front-end/ChoreList'
 import RoommateFinder from './front-end/RoommateFinder'
 import Settings from './front-end/Settings'
 import RegistrationPage from './front-end/RegistrationPage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom' 
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
     return (
             <div className="App">
-                <LoginPage />
-                <ChoreList />
-                <RoommateFinder />
-                <Settings />
-                <RegistrationPage />
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/RegistrationPage" element={<RegistrationPage />} />
+                    <Route path="/ChoreList" element={<ChoreList />} />
+                    <Route path="/RoommateFinder" element={<RoommateFinder />} />
+                    <Route path="/Settings" element={<Settings />} />
+                </Routes>
             </div>
-    )
+    );
 }
 
 export default App;
--- src/front-end/LoginPage.js
+++ src/front-end/LoginPage.js
@@ -3,8 +3,7 @@ import logo from './media/Roomie.png';
 import pic from './media/roommates.jpg';
 import './style.css';
 import './RegistrationPage.js';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import RegistrationPage from './RegistrationPage.js'
+
 
 function LoginPage() {
     const [ username, setUsername ] = useState('');
@@ -35,17 +34,11 @@ function LoginPage() {
             <br></br>
             <input className="acc-button" type="submit" id="button" value="Login" />
             </form>
-            <h4>Dont have account? <a href='/RegistrationPage.js'><u>Sign Up</u></a></h4>
+            <h4>Dont have account? <a href='/RegistrationPage'><u>Sign Up</u></a></h4>
         </div>
         <img src={pic} className='picture' alt="picture"/>
-        {/* <BrowserRouter>
-            <Routes>
-                <Route path="/RegistrationPage.js">
-                    <RegistrationPage />
-                </Route>
-            </Routes>
-        </BrowserRouter> */}
         </div>
+        
     )
 }
 
